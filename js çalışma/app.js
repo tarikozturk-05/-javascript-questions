bf1d6cf2b2d57de775436db440916971e2728b88
TarikOzturk <109246134+tarikozturk-05@users.noreply.github.com>
2022-08-16 14:09:22 +0300

    Add files via upload

---
+++ js çalışma/app.js
@@ -0,0 +1,115 @@
+// let not = +prompt("notunuzu giriniz(0-100 arası)")
+
+// if(not>=85 && not<=100){
+//     console.log("5")
+// }else if (not >=75 && not<85){
+//     console.log(4)
+// }else if (not >=65 && not <75){
+//     console.log("3")
+// }else if (not >=50 && not < 65){
+//     console.log("2")
+// }else if (not >=0 && not < 50){
+//     console.log("1")
+// }else{
+//     console.log("0-100 arasu bir değer giriniz")
+// }
+// -------------111-----------
+
+
+// let x = +prompt("lütfen bir sayı giriniz")
+
+// if(x > 0){
+//     console.log("sayınız pozitiftir")
+// }else if(x < 0){
+//     console.log("sayınız negatiftir")
+// }else if(x == 0){
+//     console.log("sayınız 0'dır")
+// }
+
+
+// -------------222-------------
+
+
+// let yas = +prompt("lütfen yaşınızı giriniz")
+
+// if(yas >= 18){
+//     console.log("ehliyet alabilirsinz")
+// }else if(yas < 18 && yas >= 0){
+//     console.log(`${18 - yas}  yıl sonra ehliyet alabilirsinz`)
+// }else {
+//     console.log("lütfen düzgün bir değer giriniz")
+// }
+// ---------------3333------
+
+// let sayi = +prompt("bir sayı giriniz")
+
+// console.log(sayi % 2 ? `sayınız tektir` : (`typeof(number sayınız çifttir)) === 'number'` ? "number sayınız çifttir" : "yanliş değer girdiniz") ) 
+
+// console.log(sayi % 2 ? "sayı tek" : (parseFloat(sayi) ? "sayi çift ": "sayi değeri gir"))
+
+
+// -------------------444444-----
+
+// let a = 
+// console.log(a%2  && a%3 && a%5 && a%7 && a%2==1 && a%a==0 && a%1==0 ? "asal" : "asal değil")
+
+// console.log( a%2  && a%3 && a%5 && a%7 ?"asal" : "asal değil")
+// ---------------
+// let a  =  ;
+// if(a == 2 || a ==3 || a==5 || a==7) {
+//     console.log("asal sayıdır");
+// }else if(a==0 || a==1){
+//     console.log("asal sayı değildir")
+// }
+// else{
+//     console.log(a%2 && a%3 && a%5 && a%7 ? "asal" : "asal değil");
+// }
+
+
+
+// var text="";
+// var sayi=5;
+ 
+// function asalHesapla()
+// {
+// var sayac=0;
+// console.log(sayi1)
+// for(i=2; i<sayi; i++)
+//  {
+// 	if(sayi%i==0)
+// 	{
+// 	sayac++;
+// 	}
+//  }
+// 	if(sayac==0)
+// 	{
+// 	text="Asal";
+// 	}
+// 	else
+// 	{
+// 	text="Asal değil.<br/> 1 ve Kendisi hariç <strong>"+sayac+"</strong> adet böleni var";
+// 	}
+//  console.log()
+//  }
+
+
+
+
+
+// let sum = 0;
+// let number = 0;
+// do {
+//     sum += number;
+//     number = parseInt(prompt('Enter a number: '));
+// } while(number >= 0)
+// console.log(`The sum is ${sum}.`);
+
+
+// var players = ["Messi ", "Ronaldo ", "Hazard ", "Neymar ", "Salah "];
+// var text = "";
+// var i;  
+// for (i = 2; i < players.length; i++) {
+// text += players[i];
+// }
+// document.write(text);
+
