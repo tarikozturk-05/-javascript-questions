4ca975cf0eaa790f0ad076ee27653b7f45f127a0
TarikOzturk <109246134+tarikozturk-05@users.noreply.github.com>
2022-08-18 14:00:15 +0300

    Add files via upload

---
+++ 18.08_js_Çalışma/app.js
@@ -0,0 +1,162 @@
+// let hak = 5; 
+// const rastgele = Math.round(Math.random() * 100); 
+// console.log(rastgele);
+
+// let tahmin;
+
+// do { tahmin = Number(prompt("Lutfen 0-100 arasinda bir tahmin giriniz:"));
+//  hak -= 1; 
+// if (tahmin === rastgele) { console.log(`Tebrikler ${5 - hak} kerede bildiniz.`) 
+
+// ; 
+//     break; }
+//  else if (tahmin < rastgele) { console.log("ARTTIR ⬆️"); }
+//   else { console.log("AZALT ⬇️"); } }
+ 
+
+//  while (hak > 0);
+
+// if (tahmin !== rastgele) { console.log("Uzgunuz oyunu kaybettiniz 😔😔"); 
+// }
+
+
+
+
+
+
+
+
+// let hak = 5;
+// const rastgele = Math.round(Math.random()*100);
+// let tahmin;
+// let istek = true;
+// console.log(rastgele)
+
+// while(istek){
+//     do{
+//         tahmin = Number(prompt("tahmin sayiyi giriniz:"));
+//         hak-=1;
+//         if(tahmin===rastgele){
+//             console.log(`Tebrikler ${5-hak} kere de bildiniz.`)
+//             break;
+//         }else if(tahmin<rastgele){
+//             console.log("Arttır")
+//         }else if(tahmin>rastgele){
+//             console.log("Azalt")
+//         }
+//     }while(hak>0);
+    
+//     if(tahmin!==rastgele){
+//         console.log("Uzgunuz oyunu kaybettiniz")
+//     }
+
+//     const tercih = prompt("Tekrar oynamak ister misiniz? E veya e giriniz!")
+//     if(tercih === "E" || tercih === 'e'){
+//         istek = true;
+//         hak=5;
+//     }else if(tercih !== 'E' || tercih !== 'e'){
+//         istek = false;
+//     }
+// }
+
+
+
+
+
+
+
+// let ortalama ;
+// let not ;
+// do{ 
+//     while(not = prompt("Notunuzu giriniz")){
+//         if(not !="q" || not !="Q"){
+//             ortalama = sayac /
+//             console.log(`${ortalama}`)
+//         }
+//         let sayac = 0 ;
+//         while(not == "Q" || not =="q"){
+        
+//         console.log(`${ortalama / i}` );
+      
+//         }
+
+//     for(let i =1 ; i<10 ;i++ ){
+    
+//         sayac += not ;
+//     console.log(`${not}`)
+   
+   
+//     ortalama = sayac / i ;
+
+//     console.log(`${ortalama}`) ;
+// }
+// }
+
+// }
+
+//* -------------------------------
+
+// let not;
+// let notToplamı=0
+// let i = 0;
+// while( not!= "Q" && not != "q"){
+//     not= prompt("notunuzu giriniz")
+//     console.log(not)
+//     if( not == "Q" || not =="q"){
+//         break;
+//     }
+//     notToplamı+= +not
+//     i += 1
+//  }
+//      console.log(`not ortalaması:`,notToplamı/i)
+
+//*---------------------------------
+// let i = 0;
+// for(let i = 0 ;i < 3; i++){
+//     let x = +prompt("sayınızı giriniz")
+//     if(x>=50 && x<100){
+//         console.log("true")
+
+//     }else if(){
+//         console.log("false")
+//         break;
+//     }
+//     } 
+
+
+
+//*-------------------------
+
+
+
+    // let x = +prompt("sayınızı giriniz");
+    // let y = +prompt("sayınızı giriniz");
+    // let z = +prompt("sayınızı giriniz");
+    // console.log(x, y ,z);
+    // console.log((x<100 && x>=50) || (y<100 && y>=50) || (z<100 && z>=50)) ;
+  
+//*-----------------------
+
+    let a;
+let değer = false
+for(i=1; i <=3 ; i++){
+   a = +prompt(`bir sayı giriniz`)
+  
+    console.log(a)
+    if(a >= 50 && a < 100){
+       değer = true 
+       console.log(değer)
+    }
+    else{
+        console.log(değer)
+     }
+}
+    
+   
+
+
+
+    
+
+
+
