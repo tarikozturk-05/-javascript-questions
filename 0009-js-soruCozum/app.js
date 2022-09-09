4f5214d6a09a6a762d3b804edf643894739b088a
TarikOzturk <109246134+tarikozturk-05@users.noreply.github.com>
2022-09-09 12:03:17 +0300

    Add files via upload

---
+++ 0009-js-soruCozum/app.js
@@ -0,0 +1,30 @@
+// let note = document.querySelector(".note");
+// console.log(note.parentNode);
+// console.log("------");
+// console.log("to get next sibling");
+// curent = document.querySelector(".current");
+// nextSiling = curent.nextElementSibling;
+// console.log(nextSiling);
+// nextSiling = curent.previousElementSibling;
+// console.log(nextSiling);
+
+// console.log("------");
+// console.log("to get all next siblings");
+// while (nextSibling) {
+//   console.log(nextSibling);
+//   nextSibling = nextSibling.nextElementSibling;
+// }
+// console.log("------");
+// console.log("to get previous sibling");
+// current = document.querySelector(".current");
+// prevSibling = currentNode.previousElementSibling;
+// console.log("------");
+// console.log("to get all previous siblings");
+// let current = document.querySelector(".current");
+// let prevSibling = current.previousElementSibling;
+// while (prevSibling) {
+//   console.log(prevSibling);
+//   prevSibling = current.previousElementSibling;
+// }
+
+// ***************
