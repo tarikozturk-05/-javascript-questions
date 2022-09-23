afe3944fd3c1b7318901809316c95848fd70ffdd
TarikOzturk <109246134+tarikozturk-05@users.noreply.github.com>
2022-09-23 14:46:23 +0300

    Add files via upload

---
+++ 0016-js-soru-cozum/app.js
@@ -0,0 +1,26 @@
+
+// function countSmileys(arr) {
+//     arr.forEach((e) => {
+//         let sayac = 0;
+//         let ab = e.split("");
+//         console.log(ab)
+    
+      
+
+//     });
+
+
+    
+// }
+// function countSmileys(arr) {
+//   return arr.reduce((n, s) => (/^[:;][-~]?[)D]$/.test(s) ? n + 1 : n), 0);
+// }
+// console.log(countSmileys([':)',':(',':D',':O',':;']))
+
+
+
+//??????? !!!!!!! ******* *****************
+function noSpace(x) {
+ return x.split(" ").join("");
+}
+console.log(noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
