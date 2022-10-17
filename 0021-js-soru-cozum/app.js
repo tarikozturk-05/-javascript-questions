fe6a040406716aad67127d5a0137fc8760a49cbc
TarikOzturk <109246134+tarikozturk-05@users.noreply.github.com>
2022-10-17 18:16:11 +0300

    Add files via upload

---
+++ 0021-js-soru-cozum/app.js
@@ -0,0 +1,14 @@
+// let arr = [5,4,3]
+// let a= arr.array.forEach(element => {
+//   object
+// });
+// console.log(a);
+
+// function logThis(){
+//   console.log(this)
+// }
+// logThis()
+
+// const arr = ["ferrrari","audi","ford","aaa"]
+// arr.splice(0,2)
+// console.log(arr);
